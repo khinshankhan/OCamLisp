5eff83445e104e967db4b8f6e8d663a346e0b360
Khinshan Khan <khanshan501@gmail.com>
2019-06-26 12:05:50 -0400

    hmm works with stdin now

--- main.ml
+++ main.ml
@@ -1,6 +1,26 @@
+open Parser
+open Lexer
+
 let filename = Sys.argv.(1)
 
+let print_token = function
+  | Parser.INT i-> print_endline ("NUMBER " ^ string_of_int i)
+  | Parser.SYM s-> print_endline ("SYMBOL " ^ s)
+  | Parser.STRING s-> print_endline ("STRING " ^ s)
+  | Parser.LPAREN -> print_endline ("LPAREN (")
+  | Parser.RPAREN -> print_endline ("RPAREN )")
+  | _ -> print_endline "something else"
+
+let rec lexer lexbuf =
+  let token = Lexer.read lexbuf in
+  begin
+    print_token token;
+    lexer lexbuf;
+  end
+
 let () =
-  let lexbuf = Lexing.from_string "(+ 5 7)" in
-  let result = Parser.prog Lexer.read lexbuf in
-  print_endline result
+  try
+    let lexbuf = Lexing.from_channel stdin in
+    lexer lexbuf
+  with
+    _ -> exit 0;
