177cae7a5c4772c659992490179b00da44850bc9
Khinshan Khan <khanshan501@gmail.com>
2019-06-25 23:30:28 -0400

    seriously no idea

--- main.ml
+++ main.ml
@@ -1,9 +1,6 @@
 let filename = Sys.argv.(1)
 
 let () =
-  let in_channel = In_channel.create filename in
-  let lexbuf = Lexing.from_channel in_channel in
-  lexbuf.lex_curr_p <-
-    { lexbuf.lex_curr_p with pos_fname = filename };
-  parse_and_print lexbuf;
-  In_channel.close in_channel;
+  let lexbuf = Lexing.from_string "(+ 5 7)" in
+  let result = Parser.prog Lexer.read lexbuf in
+  print_endline result
