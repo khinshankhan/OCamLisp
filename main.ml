8356eb6f4f6f516c125f6dbc0b591fc7364afa9d
Khinshan Khan <khanshan501@gmail.com>
2019-06-25 23:09:28 -0400

    hmm, i cant get a poc main

--- main.ml
+++ main.ml
@@ -1,13 +1,9 @@
 let filename = Sys.argv.(1)
 
 let () =
-  let inBuffer = open_in filename in
-  let lineBuffer = Lexing.from_channel inBuffer in
-  try
-    let acceptance = Parser.main Lexer.main lineBuffer in
-    match acceptance with
-    | true -> print_string "Accepted!\n"
-    | false -> print_string "Not accepted!\n"
-  with
-  | Lexer.Error msg -> Printf.fprintf stderr "%s%!\n" msg
-  | Parser.Error -> Printf.fprintf stderr "At offset %d: syntax error.\n%!" (Lexing.lexeme_start lineBuffer)
+  let in_channel = In_channel.create filename in
+  let lexbuf = Lexing.from_channel in_channel in
+  lexbuf.lex_curr_p <-
+    { lexbuf.lex_curr_p with pos_fname = filename };
+  parse_and_print lexbuf;
+  In_channel.close in_channel;
