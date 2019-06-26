f1c094f52f7af6fbf95c46fa5c6618e5a10ab1d7
Khinshan Khan <khanshan501@gmail.com>
2019-06-26 12:27:39 -0400

    works on a file now

--- main.ml
+++ main.ml
@@ -1,14 +1,13 @@
 open Parser
 open Lexer
 
-let filename = Sys.argv.(1)
-
 let print_token = function
   | Parser.INT i-> print_endline ("NUMBER " ^ string_of_int i)
   | Parser.SYM s-> print_endline ("SYMBOL " ^ s)
   | Parser.STRING s-> print_endline ("STRING " ^ s)
   | Parser.LPAREN -> print_endline ("LPAREN (")
   | Parser.RPAREN -> print_endline ("RPAREN )")
+  | Parser.EOF -> failwith "yayeet"
   | _ -> print_endline "something else"
 
 let rec lexer lexbuf =
@@ -20,7 +19,8 @@ let rec lexer lexbuf =
 
 let () =
   try
-    let lexbuf = Lexing.from_channel stdin in
+    let filename = Sys.argv.(1) in
+    let lexbuf = Lexing.from_channel (open_in filename) in
     lexer lexbuf
   with
     _ -> exit 0;
