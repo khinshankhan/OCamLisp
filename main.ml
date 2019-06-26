4f86e32b3dc5380d409893bf0d2b9dcea41d6940
Khinshan Khan <khanshan501@gmail.com>
2019-06-25 22:44:53 -0400

    show if each line was good or not

--- main.ml
+++ main.ml
@@ -1,4 +1,13 @@
 let filename = Sys.argv.(1)
 
 let () =
-  print_endline filename
+  let inBuffer = open_in filename in
+  let lineBuffer = Lexing.from_channel inBuffer in
+  try
+    let acceptance = Parser.main Lexer.main lineBuffer in
+    match acceptance with
+    | true -> print_string "Accepted!\n"
+    | false -> print_string "Not accepted!\n"
+  with
+  | Lexer.Error msg -> Printf.fprintf stderr "%s%!\n" msg
+  | Parser.Error -> Printf.fprintf stderr "At offset %d: syntax error.\n%!" (Lexing.lexeme_start lineBuffer)
