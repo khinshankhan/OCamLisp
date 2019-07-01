ef537b5bc3e62e26df2c5b4b6f018ca88aa71d6f
Khinshan Khan <khanshan501@gmail.com>
2019-07-01 14:22:17 -0400

    took out lexer debugging

--- src/main.ml
+++ src/main.ml
@@ -1,15 +1,6 @@
 open Parser
 open Lexer
 
-let print_token = function
-  | Parser.INT i-> print_endline ("NUMBER " ^ string_of_int i)
-  | Parser.SYM s-> print_endline ("SYMBOL " ^ s)
-  | Parser.STRING s-> print_endline ("STRING " ^ s)
-  | Parser.LPAREN -> print_endline ("LPAREN (")
-  | Parser.RPAREN -> print_endline ("RPAREN )")
-  | Parser.EOF -> failwith "yayeet"
-  | _ -> print_endline "something else"
-
 let matcher = function
   | `Bool b -> print_endline "b"
   | `Char c -> print_endline "c"
@@ -24,14 +15,6 @@ let matcher = function
 let rec print_sexp = function
   | Sexp.Atom t -> matcher t
   | Sexp.Cons t -> List.iter print_sexp t
-
-let rec lexer lexbuf =
-  let token = Lexer.read lexbuf in
-  begin
-    print_token token;
-    lexer lexbuf;
-  end
-
 
 let () =
   let argv_list = Array.to_list Sys.argv in
