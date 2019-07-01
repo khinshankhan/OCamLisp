fcd351334ca1fc24022c0cb2b6de94f0efcb3e94
Khinshan Khan <khanshan501@gmail.com>
2019-07-01 14:21:18 -0400

    good progress on deconstructing sexp list, but forgone repl

--- src/main.ml
+++ src/main.ml
@@ -10,12 +10,28 @@ let print_token = function
   | Parser.EOF -> failwith "yayeet"
   | _ -> print_endline "something else"
 
+let matcher = function
+  | `Bool b -> print_endline "b"
+  | `Char c -> print_endline "c"
+  | `Float f -> print_endline "f"
+  | `Int i -> print_endline "i"
+  | `String s -> print_endline "str"
+  | `Sym s -> print_endline "sym"
+  | `Tuple [] -> print_endline "empty"
+  | `Tuple ls -> print_endline "stuff in"
+
+
+let rec print_sexp = function
+  | Sexp.Atom t -> matcher t
+  | Sexp.Cons t -> List.iter print_sexp t
+
 let rec lexer lexbuf =
   let token = Lexer.read lexbuf in
   begin
     print_token token;
     lexer lexbuf;
   end
+
 
 let () =
   let argv_list = Array.to_list Sys.argv in
@@ -28,4 +44,5 @@ let () =
     end
   in
   let lexbuf = Lexing.from_channel channel_name in
-  lexer lexbuf
+  let sexp = Parser.prog Lexer.read lexbuf in
+  List.iter print_sexp sexp
