1d67660be94c8dc49b3fda6f9d572cab152a0c0e
Khinshan Khan <khanshan501@gmail.com>
2019-07-02 17:52:44 -0400

    split into interactive v noninteractive. ignoring interactive for now

--- src/main.ml
+++ src/main.ml
@@ -1,16 +1,18 @@
 open Parser
 open Lexer
 
-let () =
-  let argv_list = Array.to_list Sys.argv in
-  let channel_name =
-    begin
-      match argv_list with
-      | [a] -> stdin
-      | [a; b] -> (open_in b)
-      | _ -> failwith "Invalid number of arguments"
-    end
-  in
+let interactive =
+  ()
+
+let noninteractive filename =
+  let channel_name = open_in filename in
   let lexbuf = Lexing.from_channel channel_name in
   let sexp = Parser.prog Lexer.read lexbuf in
   List.iter Eval.print_sexp sexp
+
+let () =
+  let argv_list = Array.to_list Sys.argv in
+  match argv_list with
+  | [a] -> interactive
+  | [a; b] -> noninteractive b
+  | _ -> failwith "Invalid number of arguments"
