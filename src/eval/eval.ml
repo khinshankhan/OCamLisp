e2b99d66ac70a3c8cef0fa4ac5bfbbbe03c2039c
Khinshan Khan <khanshan501@gmail.com>
2019-07-02 18:37:37 -0400

    simple add and subtract working with ints!

--- src/eval/eval.ml
+++ src/eval/eval.ml
@@ -8,11 +8,35 @@ let matcher = function
   | `Tuple [] -> print_endline "empty"
   | `Tuple ls -> print_endline "stuff in"
 
+let sym_lookup = function
+  | `Sym s ->
+    (match s with
+     | "+" -> (+)
+     | "-" -> (-)
+     | _ -> failwith "sym fail 2")
+  | _ -> failwith "sym fail "
+
+let atomizer t =
+  match t with
+  |  Sexp.Atom t ->
+    begin
+      match t with
+      | `Int i -> i
+      | _ -> failwith "atom fail"
+    end
+  | _ -> failwith "atom fail"
 
 let rec eval_sexp = function
-  | Sexp.Atom t -> matcher t
-  | Sexp.Cons t -> List.iter eval_sexp t
+  | Sexp.Cons t ->
+    begin
+      match t with
+      | (Sexp.Atom h)::t ->
+        let a = List.map atomizer t in
+        List.fold_right (sym_lookup h) a 0
+      | _ -> failwith "cons fail"
+    end
+  | _ -> failwith "sexp fail"
 
 let rec eval = function
-  | h::t -> eval_sexp h; print_endline ""; eval t
+  | h::t -> print_int (eval_sexp h); print_endline ""; eval t
   | _ -> ()
