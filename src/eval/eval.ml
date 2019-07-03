448594f574e3e1606a1c1fcacfa7ed36883348cd
Khinshan Khan <khanshan501@gmail.com>
2019-07-03 16:41:48 -0400

    getting closer, just need to properly account for wrapping now

--- src/eval/eval.ml
+++ src/eval/eval.ml
@@ -1,12 +1,32 @@
+type wrapper =
+    Bool of bool
+  | Char of char
+  | Float of float
+  | Int of int
+  | String of string
+  | Sym of string
+  | Tuple of wrapper list
+  | Nil
+
+let rec convert = function
+  | `Bool b -> Bool b
+  | `Char c -> Char c
+  | `Float f -> Float f
+  | `Int i -> Int i
+  | `String s -> String s
+  | `Sym s -> Sym s
+  | `Tuple [] -> Nil
+  | `Tuple ls -> Tuple (List.map convert ls)
+
 let matcher = function
-  | `Bool b -> print_endline "b"
-  | `Char c -> print_endline "c"
-  | `Float f -> print_endline "f"
-  | `Int i -> print_endline "i"
-  | `String s -> print_endline "str"
-  | `Sym s -> print_endline "sym"
-  | `Tuple [] -> print_endline "empty"
-  | `Tuple ls -> print_endline "stuff in"
+  | Bool b -> print_endline "b"
+  | Char c -> print_endline "c"
+  | Float f -> print_endline "f"
+  | Int i -> print_endline "i"
+  | String s -> print_endline "str"
+  | Sym s -> print_endline "sym"
+  | Nil -> print_endline "empty"
+  | Tuple ls -> print_endline "stuff in"
 
 let sym_lookup = function
   | `Sym s ->
@@ -28,14 +48,9 @@ let rec eval_sexp = function
   | _ -> failwith "sexp fail"
 and atomizer t =
   match t with
-  | Sexp.Atom t ->
-    begin
-      match t with
-      | `Int i -> i
-      | _ -> failwith "atom fail"
-    end
+  | Sexp.Atom t -> convert t
   | _ -> eval_sexp t
 
 let rec eval = function
-  | h::t -> print_int (eval_sexp h); print_endline ""; eval t
+  | h::t -> matcher (eval_sexp h); print_endline ""; eval t
   | _ -> ()
