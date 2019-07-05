e311b72475b8e83ee097db5d30e947ba61288106
Khinshan Khan <khanshan501@gmail.com>
2019-07-05 00:31:15 -0400

    took out useless wrappers

--- src/eval/eval.ml
+++ src/eval/eval.ml
@@ -2,10 +2,10 @@ open Syntax
 
 let op f1 f2 n1 n2 =
   match n1, n2 with
-  | Int x, Int y -> Int (f1 x y)
-  | Int x, Float y -> Float (f2 (float x) y)
-  | Float x, Int y -> Float (f2 x (float y))
-  | Float x, Float y -> Float (f2 x y)
+  | `Int x, `Int y -> `Int (f1 x y)
+  | `Int x, `Float y -> `Float (f2 (float x) y)
+  | `Float x, `Int y -> `Float (f2 x (float y))
+  | `Float x, `Float y -> `Float (f2 x y)
   | _ -> failwith "invalid num"
 
 let sym_lookup = function
@@ -22,13 +22,13 @@ let rec eval_sexp = function
       match t with
       | (Sexp.Atom h)::t ->
         let a = List.map atomizer t in
-        List.fold_right (sym_lookup h) a (Int 0)
+        List.fold_right (sym_lookup h) a (`Int 0)
       | _ -> failwith "cons fail"
     end
   | _ -> failwith "sexp fail"
 and atomizer t =
   match t with
-  | Sexp.Atom t -> sym_to_t t
+  | Sexp.Atom t -> t
   | _ -> eval_sexp t
 
 let rec eval = function
--- src/syntax/syntax.ml
+++ src/syntax/syntax.ml
@@ -1,27 +1,9 @@
-type t =
-    Bool of bool
-  | Char of char
-  | Float of float
-  | Int of int
-  | String of string
-  | Sym of string
-  | Nil
-
-let rec sym_to_t = function
-  | `Bool b -> Bool b
-  | `Char c -> Char c
-  | `Float f -> Float f
-  | `Int i -> Int i
-  | `String s -> String s
-  | `Sym s -> Sym s
-  | `Tuple [] -> Nil
-  | _ -> failwith "unaccounted for"
-
 let print_t = function
-  | Bool b -> print_endline "b"
-  | Char c -> print_char c
-  | Float f -> print_float f
-  | Int i -> print_int i
-  | String s -> print_endline "str"
-  | Sym s -> print_endline "sym"
-  | Nil -> print_endline "empty"
+  | `Bool b -> print_endline "b"
+  | `Char c -> print_char c
+  | `Float f -> print_float f
+  | `Int i -> print_int i
+  | `String s -> print_endline "str"
+  | `Sym s -> print_endline "sym"
+  | `Tuple [] -> print_endline "empty"
+  | _ -> failwith "unaccounted for"
