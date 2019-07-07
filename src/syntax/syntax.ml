547626f5632e107758e2f7a07efa0574cc983db0
Khinshan Khan <khanshan501@gmail.com>
2019-07-07 12:58:14 -0400

    better printing

--- src/syntax/syntax.ml
+++ src/syntax/syntax.ml
@@ -3,13 +3,18 @@ let sym_extract = function
   | _ -> failwith "unaccounted for"
 
 let print_t = function
-  | `Bool b -> print_endline "bool"
+  | `Bool b ->
+    begin
+      match b with
+      | true -> print_endline "#t"
+      | false -> print_endline "#f"
+    end
   | `Char c -> print_char c
   | `Float f -> print_float f
   | `Int i -> print_int i
   | `String s -> Printf.printf "\"%s\"" s
-  | `Sym s -> print_endline "sym"
-  | `Tuple [] -> print_endline "empty"
+  | `Sym s -> print_endline s
+  | `Tuple [] -> print_endline "Nil"
   | _ -> failwith "unaccounted for"
 
 let print t =
