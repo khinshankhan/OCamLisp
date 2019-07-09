aa986057c55586391af547ea7220ec49ba1f2b0a
Khinshan Khan <khanshan501@gmail.com>
2019-07-09 17:18:34 -0400

    option type is cleaner than token typing

--- src/reader/reader.ml
+++ src/reader/reader.ml
@@ -13,13 +13,12 @@ let rec interactive env =
         flush stdout;
         let env = IO.read_line IO.stdin |> IO.input_string |> Lexing.from_input |> Parser.prog Lexer.read |> Eval.eval
         in
-        `Tuple env
+        Some env
       with
-      | BatInnerIO.No_more_input -> print_endline ""; `String "logout"
-      | Failure msg -> `String msg
+      | BatInnerIO.No_more_input -> Printf.printf "\nlogout\n"; None
+      | Failure msg -> print_endline msg; None
     end
   in
   match interactive_wrapper env with
-  | `Tuple env -> interactive env
-  | `String msg -> print_endline msg
-  | _ -> print_endline "Something went horribly wrong"
+  | Some env -> interactive env
+  | None -> ()
